f0d22da80772de172781b75e3a8e496d53f4398f
terry <terry@terry-openstack.(none)>
2013-10-25 16:17:16 -0700

    pass image parameter from arguments

--- createinstance.py
+++ createinstance.py
@@ -12,7 +12,7 @@ def createinstance():
 
     neutron = client.Client(username='admin', password='supersecret', tenant_name='admin', auth_url=os.environ['OS_AUTH_URL'])
     neutron.format= 'json'
-    netname = str(argv[2])
+    netname = str(argv[3])
 
     print netname
 
@@ -23,10 +23,10 @@ def createinstance():
     else:
         netid =  neutron.list_networks(name=netname)["networks"][0]["id"]
 
-
-    image = nova.images.find(name="cirros-0.3.1-x86_64-uec")
-    flavor = nova.flavors.find(name="m1.tiny")
-    instancename = str(argv[1])
+    imagename = str(argv[1])
+    image = nova.images.find(name=imagename)
+    flavor = nova.flavors.find(name="ubuntu")
+    instancename = str(argv[2])
     networkinfo =[{'net-id':netid}]
     
     print "new instance named", instancename,  "will be created"
@@ -46,8 +46,8 @@ def createinstance():
 
 if __name__ == "__main__":
     
-    if len(argv)!=3:
-        print "this scription takes exactly 2 argument, usage: ./createinstance.py instancename networkname"
+    if len(argv)!=4:
+        print "this scription takes exactly 3 argument, usage: ./createinstance.py imagename instancename networkname"
         exit(0)
     else:    
         createinstance()
